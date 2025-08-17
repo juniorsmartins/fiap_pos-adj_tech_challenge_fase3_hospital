4bc3d30e3e5bff3d9c5a245a1722e6a9a042b17c
juniorsmartins <junior.soares.martins@proton.me>
2025-08-17 09:49:50 -0300

    REFACTOR: config httpBasic.

--- src/main/java/fiap/adj/fase3/tech_challenge_hospital/application/configs/SecurityConfig.java
+++ src/main/java/fiap/adj/fase3/tech_challenge_hospital/application/configs/SecurityConfig.java
@@ -1,47 +1,37 @@
 package fiap.adj.fase3.tech_challenge_hospital.application.configs;
 
-import fiap.adj.fase3.tech_challenge_hospital.application.configs.security.MyUserDetailService;
 import fiap.adj.fase3.tech_challenge_hospital.application.configs.security.PlainTextPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // Define como será o acesso aos dados dos usuários
-    private final MyUserDetailService myUserDetailService;
-
-    public SecurityConfig(MyUserDetailService myUserDetailService) {
-        this.myUserDetailService = myUserDetailService; // Injeta serviço de usuários
-    }
-
     // Configura gerenciador de autenticação
+    // HttpSecurity - trata da camada de segurança do Http
+    // O HttpSecurity muda a configuração padrão para Http Basic
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager(); // Configuração padrão
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 
     // Define como será a codificação do password
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PlainTextPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authentication = new DaoAuthenticationProvider();
-        authentication.setUserDetailsService(myUserDetailService);
-        authentication.setPasswordEncoder(passwordEncoder());
-        return authentication;
     }
 
     // SecurityFilterChain - Sequência de filtros de URL para segurança
@@ -53,7 +43,25 @@ public class SecurityConfig {
             authorizeRequests.requestMatchers("/logout").permitAll();
             authorizeRequests.anyRequest().authenticated();
         });
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(
+                User.withUsername("usuario")
+                        .password("senha")
+                        .roles("USER")
+                        .build()
+        );
+        inMemoryUserDetailsManager.createUser(
+                User.withUsername("admin")
+                        .password("password")
+                        .roles("ADMIN", "USER")
+                        .build()
+        );
+        return inMemoryUserDetailsManager;
     }
 }
