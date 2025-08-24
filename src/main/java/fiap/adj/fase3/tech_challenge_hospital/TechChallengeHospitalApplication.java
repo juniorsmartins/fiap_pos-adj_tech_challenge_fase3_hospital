b95c92066ad2a057d3c882aed4e4d1992df55458
juniorsmartins <junior.soares.martins@proton.me>
2025-08-24 06:41:20 -0300

    ...

--- src/main/java/fiap/adj/fase3/tech_challenge_hospital/TechChallengeHospitalApplication.java
+++ src/main/java/fiap/adj/fase3/tech_challenge_hospital/TechChallengeHospitalApplication.java
@@ -1,13 +1,14 @@
 package fiap.adj.fase3.tech_challenge_hospital;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TechChallengeHospitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechChallengeHospitalApplication.class, args);
 	}
-
 }
